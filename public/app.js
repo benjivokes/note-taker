e6744ec7ea2a029b81f7c68dd80524c4058473c7
Benjamin Vokes <ben.mym.ac>
2020-01-13 14:44:12 -0800

    Created/edited app.js.

---
+++ public/app.js
@@ -0,0 +1,88 @@
+function getResults() {
+    $("#results").empty();
+    $.getJSON("/all", function(data) {
+      for (var i = 0; i < data.length; i++) {
+        $("#results").prepend("<p class='dataentry' data-id=" + data[i]._id + "><span class='dataTitle' data-id=" +
+          data[i]._id + ">" + data[i].title + "</span><span class=deleter>X</span></p>");
+      }
+    });
+  }
+  getResults();
+
+  $(document).on("click", "#makenew", function() {
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: "/submit",
+      data: {
+        title: $("#title").val(),
+        note: $("#note").val(),
+        created: Date.now()
+      }
+    })
+    .done(function(data) {
+      $("#results").prepend("<p class='dataentry' data-id=" + data._id + "><span class='dataTitle' data-id=" +
+        data._id + ">" + data.title + "</span><span class=deleter>X</span></p>");
+      $("#note").val("");
+      $("#title").val("");
+    }
+    );
+  });
+  
+  $("#clearall").on("click", function() {
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: "/clearall",
+      success: function(response) {
+        $("#results").empty();
+      }
+    });
+  });
+  
+  $(document).on("click", ".deleter", function() {
+    var selected = $(this).parent();
+    $.ajax({
+      type: "GET",
+      url: "/delete/" + selected.attr("data-id"),
+  
+      success: function(response) {
+        selected.remove();
+        $("#note").val("");
+        $("#title").val("");
+        $("#actionbutton").html("<button id='makenew'>Submit</button>");
+      }
+    });
+  });
+  
+  $(document).on("click", ".dataTitle", function() {
+    var selected = $(this);
+    $.ajax({
+      type: "GET",
+      url: "/find/" + selected.attr("data-id"),
+      success: function(data) {
+        $("#note").val(data.note);
+        $("#title").val(data.title);
+        $("#actionbutton").html("<button id='updater' data-id='" + data._id + "'>Update</button>");
+      }
+    });
+  });
+
+  $(document).on("click", "#updater", function() {
+    var selected = $(this);
+    $.ajax({
+      type: "POST",
+      url: "/update/" + selected.attr("data-id"),
+      dataType: "json",
+      data: {
+        title: $("#title").val(),
+        note: $("#note").val()
+      },
+      success: function(data) {
+        $("#note").val("");
+        $("#title").val("");
+        $("#actionbutton").html("<button id='makenew'>Submit</button>");
+        getResults();
+      }
+    });
+  });
